8c6e02e418080f4e536e63e614c5e7f45b9b53eb
Brian Binder <bbinder912@gmail.com>
2018-03-05 15:21:31 -0500

    Clean up app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
@@ -8,13 +8,6 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { GeoPage} from '../pages/geo/geo';
-
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { TouchPunchProvider } from '../providers/touchPunch';
-import { WordGraphMakerProvider } from '../providers/wordGraphMaker';
-import { Grid } from '../providers/grid';
 
 import { AppProviders } from './app.providers';
 
